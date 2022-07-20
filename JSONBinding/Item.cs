1dfc13e36a57668c9878a2dd1e1cc7ae321ce86a
UlakS <srishti@codebee.dk>
2022-07-20 09:50:39 +0545

    Adding JSON data convertor

---
+++ JSONBinding/Item.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONBinding
+{
+    public class Item
+    {
+        public int item_id { get; set; }
+        public string item_name { get; set; }
+        public double item_price { get; set; }
+        public int category_id { get; set; }
+        public string kot_print { get; set; }
+        public string applicable_time_period { get; set; }
+        public string image_color { get; set; }
+        public double item_cost_price { get; set; }
+        public string image_name { get; set; }
+        public double discount { get; set; }
+        public string barcode { get; set; }
+        public int category_id1 { get; set; }
+        public string category_name { get; set; }
+        public string category_color { get; set; }
+        public string applicable_time { get; set; }
+        public int id { get; set; }
+        private readonly string _path = $"D:\\Smtech\\JSON\\JSONBinding\\JSONBinding\\JSONFile\\item.json";
+        public List<Item> GetList()
+        {
+            string jsonFromFile;
+            using (var reader = new StreamReader(_path))
+            {
+                jsonFromFile = reader.ReadToEnd();
+            }
+            if (jsonFromFile != null)
+            {
+                List<Item> itemlist = JsonConvert.DeserializeObject<List<Item>>(jsonFromFile);
+                return itemlist;
+            }
+            return null;
+        }
+
+    }
+
+
+
+
+}
---
+++ JSONBinding/ItemList.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JSONBinding
+{
+    public partial class ItemList : Form
+    {
+        Item de = new Item();
+        public ItemList()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ItemList_Load(object sender, EventArgs e)
+        {
+            
+                dataGridViewItems.DataSource = de.GetList();
+
+            
+        }
+    }
+}
